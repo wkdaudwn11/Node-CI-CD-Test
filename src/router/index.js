0e3ff0e72e63762c9e2fcd0ff0de6e5e92267e99
JangMyungJoo <wkdaudwn1028@gmail.com>
2020-02-11 15:24:03 +0900

    commit test3

--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,7 @@ const { getUsers, getUser } = require("../controller");
 
 const router = new Router();
 
-router.get("/", ctx => (ctx.body = "안녕하세요, 장명주입니다!!"));
+router.get("/", ctx => (ctx.body = "안녕하세요, 장명주입니다!!!!"));
 router.get("/deploy", ctx => (ctx.body = "무중단배포 성공"));
 
 router.get("/users", getUsers);
